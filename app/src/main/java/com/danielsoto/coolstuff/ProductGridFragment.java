8ab6861074df1ade6ffdbec70cb2932d6e232a11
Daniel Soto <dasg.1993@gmail.com>
2021-11-17 13:58:21 -0500

    [feature] - create menu & toobar layout & implement style widget action bar

--- app/src/main/java/com/danielsoto/coolstuff/ProductGridFragment.java
+++ app/src/main/java/com/danielsoto/coolstuff/ProductGridFragment.java
@@ -2,17 +2,43 @@ package com.danielsoto.coolstuff;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 
 public class ProductGridFragment extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
     @Override
     public View onCreateView(
             @NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState
             ) {
-        return inflater.inflate(R.layout.product_grid_fragment, container, false);
+        View view = inflater.inflate(R.layout.product_grid_fragment, container, false);
+
+        setUpToolbar(view);
+        return view;
+    }
+
+    private void setUpToolbar(View view) {
+        Toolbar toolbar = view.findViewById(R.id.app_bar);
+        AppCompatActivity activity = (AppCompatActivity) getActivity();
+        if(activity != null) {
+            activity.setSupportActionBar(toolbar);
+        }
+    }
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater menuInflater) {
+         menuInflater.inflate(R.menu.toolbar_menu, menu);
+         super.onCreateOptionsMenu(menu, menuInflater);
     }
 }
